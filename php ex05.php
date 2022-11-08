f2122c9540bd69be1f8e8b2e661ac73d9dd49f27
Florian JACOB <florian.jacob96@gmail.com>
2022-11-08 16:53:46 +0100

    last commit

--- php ex05.php
+++ php ex05.php
@@ -2,15 +2,15 @@
 
 <?php
 
-function afficherInput($var) {
+function afficherInput($inputTab) {
     echo '<form action="reponse.php" method="GET">';
-    foreach ($var as $va) {
-        echo '<p>Votre ' .$va. ' : </p><br><input type="text" name="'.$va. '"><br>';
+    foreach ($inputTab as $input) {
+        echo '<p>Votre ' .$input. ' : </p><br><input type="text" name="'.$input. '"><br>';
     }
     echo '<input type=submit value="Envoyer"></form>'; 
-};
+}
 
-$nomsImput = [
+$nomsInput = [
     "Nom",
     "Prenom",
     "Ville"
