a4a9ac409b0f405547d8ec329df3faeae7c80219
Florian JACOB <florian.jacob96@gmail.com>
2022-11-10 10:10:18 +0100

    11 end

--- php ex11.php
+++ php ex11.php
@@ -2,27 +2,66 @@
 
 <?php
 
-formaterDateFr("2018-02-23");
+//- - Fonction
+function getdatenottheoneondefault($date) {
+
+    // converti Jour (en) -> (fr)
+    if ($date->format('D') == "Mon") {
+        echo "Lundi ";
+    } else if ($date->format('D') == "Tue") {
+        echo "Mardi ";
+    } else if ($date->format('D') == "Wed") {
+        echo "Mercredi ";
+    } else if ($date->format('D') == "Thu") {
+        echo "Jeudi ";
+    } else if ($date->format('D') == "Fri") {
+        echo "Vendredi ";
+    } else if ($date->format('D') == "Sat") {
+        echo "Samedi ";
+    } else if ($date->format('D') == "Sun") {
+        echo "Dimanche ";
+    };
+
+    echo $date->format('d');
+
+    // converti Mois (en) -> (fr)
+    if ($date->format('m') == "01") {
+        echo " Janvier ";
+    } else if ($date->format('m') == "02") {
+        echo " Fevrier ";
+    } else if ($date->format('m') == "03") {
+        echo " Mars ";
+    } else if ($date->format('m') == "04") {
+        echo " Avril ";
+    } else if ($date->format('m') == "05") {
+        echo " Mai ";
+    } else if ($date->format('m') == "06") {
+        echo " Juin ";
+    } else if ($date->format('m') == "07") {
+        echo " Juillet ";
+    } else if ($date->format('m') == "08") {
+        echo " Août ";
+    } else if ($date->format('m') == "09") {
+        echo " Septembre ";
+    } else if ($date->format('m') == "10") {
+        echo " Octobre ";
+    } else if ($date->format('m') == "11") {
+        echo " Novembre ";
+    } else {
+        echo " Décembre ";
+    }
+
+    echo $date->format('Y');
+}
+
+//- - Déclare une date
+$exempledate=date_create("2018-02-23");
+
+//- - - - -
+getdatenottheoneondefault($exempledate);
 
 
 
 
-//------- ex Web ---------------------
-// Date en français
-$jour = getdate();
-print_r(getdate());
-$semaine = array(" Dimanche "," Lundi "," Mardi "," Mercredi "," Jeudi ",
-" vendredi "," samedi ");
-$mois =array(1=>" janvier "," février "," mars "," avril "," mai "," juin ",
-" juillet "," août "," septembre "," octobre "," novembre "," décembre ");
-// Avec getdate()
-echo "Français : Avec getdate() : Aujourd'hui ", $semaine[$jour['wday']]
-,$jour['mday'], $mois[$jour['mon']], $jour['year'],"
-";
-// Avec date()
-echo "Français : Avec date() : Aujourd'hui ",
-$semaine[date('w')] ," ",date('j'),"", $mois[date('n')], date('Y'),"
-";
-//-----------------------------------
 
 ?>
